cff5b4c41ee93676ff02ac7f311989bddc62b2d3
10yan\641 <932632886@qq.com>
2025-05-06 16:40:17 +0800

    游戏开始界面

---
+++ Assets/MySystem/Start.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // 跳转到新游戏场景，比如“GameScene”
+        SceneManager.LoadScene("Fire");
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene("Fire");
+    }
+
+   //public void ShowCredits()
+   //{
+   //    // 跳转到开发人员界面
+   //    SceneManager.LoadScene("CreditsScene");
+   //}
+}
